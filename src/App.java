37dbe11cfcbdb29e9badc32931ceccca500baa54
dharahadi <147434080+dharahadi@users.noreply.github.com>
2025-03-10 18:35:34 +0000

    econo questao 5

--- src/App.java
+++ src/App.java
@@ -4,11 +4,13 @@ public class App {
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Esportivo("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
         Carro utilitario = new Utilitario("Utilitario", TipoCombustivel.DIESEL, 5, 70);
+        Carro econo = new Econo();
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
         System.out.println(esportivo);
         System.out.println(utilitario);
+        System.out.println(econo);
 
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
@@ -28,5 +30,14 @@ public class App {
         utilitario.viaja(300);
         utilitario.viaja(150);
         System.out.println(utilitario);
+
+        System.out.println("\n\n----------------");
+        System.out.println("\nAbastencendo carro econo com gasolina");
+        econo.abastece(TipoCombustivel.GASOLINA, 55);
+        System.out.println(econo);
+        System.out.println("\nViajando com o carro econo");
+        econo.viaja(3000);
+        econo.viaja(10000);
+
     }
 }
